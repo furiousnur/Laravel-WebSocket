e37cddb4df21633fdd9d62ab1f847b7afe6e0a9c
Nur Alam <furiousnur@gmail.com>
2019-10-26 01:13:39 +0600

    configuration websocekt with laravel echo

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,9 +37,23 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                'encrypted' => true,
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'http',
             ],
         ],
+
+        /*'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true,
+            ],
+        ],*/
 
         'redis' => [
             'driver' => 'redis',
